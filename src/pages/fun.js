b0005db0002aebe555fc22bd24bb853e8457abf4
Zack <32884367+zackeaton@users.noreply.github.com>
2021-07-23 13:58:43 -0400

    Update fun.js

--- src/pages/fun.js
+++ src/pages/fun.js
@@ -5,6 +5,7 @@ const FunPage = () => {
   return (
     <Layout pageTitle="Just For Fun">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <iframe src="https://cdn.flipsnack.com/widget/v2/widget.html?hash=hmfrismxu3" width="100%" height="480" seamless="seamless" scrolling="no" frameBorder="0" allowFullScreen></iframe>
     </Layout>
   )
 }

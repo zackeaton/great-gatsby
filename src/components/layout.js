f574bf610bec50faa3f294a8a249473dd8d7991b
Zack <32884367+zackeaton@users.noreply.github.com>
2021-07-23 10:53:28 -0400

    Added Fun Page

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,6 +24,11 @@ const Layout = ({ pageTitle, children }) => {
               About
             </Link>
           </li>
+          <li className={navLinkItem}>
+            <Link to="/fun" className={navLinkText}>
+              Fun
+            </Link>
+          </li>
         </ul>
       </nav>
       <h1 className={heading}>{pageTitle}</h1>
---
+++ src/pages/fun.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const FunPage = () => {
+  return (
+    <Layout pageTitle="Just For Fun">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+}
+
+export default FunPage
